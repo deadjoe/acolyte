b0de0ce1d670d09dd9443e3f5df128e4b21180e7
Joe <smartjoe@gmail.com>
2025-04-27 20:13:08 +0800

    Improve header layout by adding more padding around the logo

--- acolyte/web/src/components/layout/Header.tsx
+++ acolyte/web/src/components/layout/Header.tsx
@@ -18,9 +18,9 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
-        <div className="mr-4 flex">
-          <Link to="/" className="flex items-center space-x-2">
+      <div className="container flex h-14 items-center px-6">
+        <div className="mr-6 flex">
+          <Link to="/" className="flex items-center space-x-2 pl-2">
             <span className="font-bold text-xl">Acolyte</span>
           </Link>
         </div>
